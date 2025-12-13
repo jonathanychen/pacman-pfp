f0784b3d954ed47600c28190373269b1ab6d814d
kwang3704 <80053043+kwang3704@users.noreply.github.com>
2025-12-13 17:54:50 -0500

    smarter ghost pathfinding/trapping

--- src/GameExecution.hs
+++ src/GameExecution.hs
@@ -7,7 +7,11 @@ module GameExecution
 import Types
 import qualified Data.Vector as V
 import Data.Vector (Vector)
-import Data.Maybe (isJust)
+import Data.Maybe (isJust, listToMaybe, mapMaybe)
+import qualified Data.Set as Set
+import qualified Data.Map.Strict as Map
+import Data.List (sortBy, minimumBy)
+import Data.Ord (comparing)
 
 executeAction :: GameState -> Action -> (GameState, Double)
 executeAction state action =
@@ -70,6 +74,45 @@ getPelletReward state =
         Pellet -> (state, 10.0)
         _ -> (state, -0.1)
 
+findPathBFS :: Vector (Vector Cell) -> Position -> Position -> [Position] -> Maybe [Position]
+findPathBFS grid start goal occupiedPositions
+    | start == goal = Just [goal]
+    | otherwise = bfs [start] (Set.singleton start) (Map.singleton start Nothing)
+    where
+        bfs [] _ _ = Nothing
+        bfs (current:queue) visited parents
+            | current == goal = Just (reconstructPath current parents)
+            | otherwise =
+                let neighbors = getValidNeighbors grid current occupiedPositions
+                    unvisited = filter (`Set.notMember` visited) neighbors
+                    newVisited = foldr Set.insert visited unvisited
+                    newParents = foldr (\n -> Map.insert n (Just current)) parents unvisited
+                    newQueue = queue ++ unvisited
+                in bfs newQueue newVisited newParents
+        
+        reconstructPath pos parents =
+            case Map.lookup pos parents of
+                Nothing -> [pos]
+                Just Nothing -> [pos]
+                Just (Just parent) -> reconstructPath parent parents ++ [pos]
+
+getValidNeighbors :: Vector (Vector Cell) -> Position -> [Position] -> [Position]
+getValidNeighbors grid pos occupiedPositions =
+    let neighbors = [applyAction pos a | a <- allActions]
+    in filter (\p -> isValidPosition grid p && p `notElem` occupiedPositions) neighbors
+
+getBlockedDirections :: Position -> [Position] -> Vector (Vector Cell) -> [Action]
+getBlockedDirections pacPos ghostPositions grid =
+    let checkBlocked action =
+            let targetPos = applyAction pacPos action
+            in not (isValidPosition grid targetPos) || targetPos `elem` ghostPositions
+    in filter checkBlocked allActions
+
+getTrapTargets :: Position -> Vector (Vector Cell) -> [Position]
+getTrapTargets pacPos grid =
+    let targets = [applyAction pacPos a | a <- allActions]
+    in filter (isValidPosition grid) targets
+
 moveGhosts :: GameState -> GameState
 moveGhosts state = state { ghostPositions = newPositions }
     where
@@ -77,23 +120,39 @@ moveGhosts state = state { ghostPositions = newPositions }
         g = grid state
         oldPositions = ghostPositions state
         
-        newPositions = moveGhostsSequentially oldPositions []
+        trapTargets = getTrapTargets pacPos g
         
-        moveGhostsSequentially [] acc = reverse acc
-        moveGhostsSequentially (gPos:rest) acc = 
+        newPositions = moveGhostsCoordinated oldPositions trapTargets [] 0
+        
+        moveGhostsCoordinated [] _ acc _ = reverse acc
+        moveGhostsCoordinated (gPos:rest) targets acc ghostIndex =
             let occupiedPositions = acc ++ rest
-                newPos = moveGhost gPos occupiedPositions
-            in moveGhostsSequentially rest (newPos : acc)
+                newPos = if ghostIndex < 2
+                         then moveGhostChase gPos occupiedPositions
+                         else moveGhostTrap gPos targets occupiedPositions
+            in moveGhostsCoordinated rest targets (newPos : acc) (ghostIndex + 1)
         
-        moveGhost gPos occupiedPositions = 
+        moveGhostChase gPos occupiedPositions =
+            case findPathBFS g gPos pacPos occupiedPositions of
+                Just path | length path > 1 -> path !! 1
+                _ -> moveGhostSimple gPos occupiedPositions
+        
+        moveGhostTrap gPos targets occupiedPositions =
+            let targetDistances = [(t, manhattan gPos t) | t <- targets]
+                sortedTargets = map fst $ sortBy (comparing snd) targetDistances
+            in case sortedTargets of
+                [] -> moveGhostSimple gPos occupiedPositions
+                (target:_) -> case findPathBFS g gPos target occupiedPositions of
+                    Just path | length path > 1 -> path !! 1
+                    _ -> moveGhostSimple gPos occupiedPositions
+        
+        moveGhostSimple gPos occupiedPositions =
             let possibleMoves = [applyAction gPos a | a <- allActions]
                 validMoves = filter (\p -> isValidPosition g p && p `notElem` occupiedPositions) possibleMoves
-                closerMoves = filter (\p -> dist p pacPos < dist gPos pacPos) validMoves
+                closerMoves = filter (\p -> manhattan p pacPos < manhattan gPos pacPos) validMoves
             in if null closerMoves 
                 then if null validMoves then gPos else head validMoves
                 else head closerMoves
-        
-        dist (x1, y1) (x2, y2) = abs (x1 - x2) + abs (y1 - y2)
 
 updateGrid :: Vector (Vector Cell) -> Position -> Cell -> Vector (Vector Cell)
 updateGrid g (x, y) cell = g V.// [(y, (g V.! y) V.// [(x, cell)])]
